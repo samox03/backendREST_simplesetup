8cca729bf354b9d1055940e94603b025cc69bfed
samox_ <samirasteinmayer@web.de>
2022-04-16 12:07:07 +0200

    adjusted project user model

--- src/models/Test.model.author.ts
+++ src/models/Test.model.author.ts
@@ -1,11 +1,13 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
+// Create an interface representing a document in MongoDB.
 export interface iAuthor {
     name: string;
 }
 
 export interface iAuthorModel extends iAuthor, Document {}
 
+// Create a Schema corresponding to the document interface.
 const AuthorSchema: Schema = new Schema(
     {
         name: { type: String, required: true }
@@ -15,4 +17,5 @@ const AuthorSchema: Schema = new Schema(
     }
 );
 
+// Create a Model
 export default mongoose.model<iAuthorModel>('Author', AuthorSchema);
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,7 +1,8 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-export interface User {
-    id: String; // echoed from path parameter
+// Create an interface representing a document in MongoDB.
+export interface IUser {
+    idUser: String; // echoed from path parameter
     group: String; //                   // echoed from path parameter
     createdAt: Date; // first registered heartbeat
     updatedAt: Date; // last registered heartbeat
@@ -11,4 +12,21 @@ export interface User {
     };
 }
 
-export interface UserModel extends User, Document {}
+export interface IUserModel extends IUser, Document {}
+
+// Create a Schema corresponding to the document interface.
+const UserSchema: Schema = new Schema({
+    idUser: { type: String, required: true },
+    group: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    updatedAt: Date,
+    meta: {
+        // meta echoed from request body
+        foo: Number
+    }
+});
+
+// Create a Model.
+export default mongoose.model<IUserModel>('User', UserSchema);
+
+///// Connect to MongoDB..:)
